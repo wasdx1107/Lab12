d4256498e1bed9b32bd6032f96afb2421bac101f
shark <wasdx1107@gmail.com>
2018-12-20 15:12:03 +0800

    finish

---
+++ app/src/main/java/com/example/user/lab12/Data.java
@@ -0,0 +1,15 @@
+package com.example.user.lab12;
+
+class Data {
+    Result result;
+
+    class Result{
+            Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+
+        }
+
+    }
+}
